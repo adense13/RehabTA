6b510b0a12162f9373468aa6a0eff4adadf139c1
tobiasdolsson <tobias.d.olsson@hotmail.com>
2017-11-03 09:49:38 +0100

    Test commit

--- app/src/main/java/com/example/adrian/vibrationapp/MainActivity.java
+++ app/src/main/java/com/example/adrian/vibrationapp/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     Vibrator vibrator;
     boolean isVibrating = false;
 
+    //Test commit, hej Adde
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

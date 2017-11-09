02f8e1c99fd49df070157986ac17c6e343b004e0
Adrian <adense13@gmail.com>
2017-11-09 11:02:01 +0100

    Revert "Cleaned up some code"
    This reverts commit d4a12b11d5d73304f016e0806efe715669986839.
    
    # Conflicts:
    #	app/src/main/java/com/example/adrian/vibrationapp/MainActivity.java

--- app/src/main/java/com/example/adrian/vibrationapp/MainActivity.java
+++ app/src/main/java/com/example/adrian/vibrationapp/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.adrian.vibrationapp;
 
-import android.Manifest;
-import android.app.Activity;
+//import android.graphics.Camera;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     Vibrator vibrator;
     boolean isVibrating = false;
     boolean isLight = false;
+    boolean cameraOpen = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +29,9 @@ public class MainActivity extends AppCompatActivity {
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
         //---BUTTONS--///
-
         Button btn_pattern1 = findViewById(R.id.btn_pattern1);
         btn_pattern1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Custom pattern
                 long[] pattern = {3000, 1000, 2000, 1000, 2000, 1000, 500, 1000, 500, 1000, 500, 1000};
                 vibrate(pattern);
             }
@@ -44,34 +40,34 @@ public class MainActivity extends AppCompatActivity {
         Button btn_pattern2 = findViewById(R.id.btn_pattern2);
         btn_pattern2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //SOS pattern
                 long[] pattern = {0, 500, 50, 500, 50, 500, 1000, 1000, 50, 1000, 50, 1000, 1000, 500, 50, 500, 50, 500, 1000};
                 vibrate(pattern);
             }
         });
 
         Button btn_flashlight = findViewById(R.id.Flashlight1);
-
-        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 1);
-
+        final Camera cam = Camera.open();
+        final Camera.Parameters parameters = cam.getParameters();
         btn_flashlight.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                final Camera cam = Camera.open();
-                final Camera.Parameters parameters = cam.getParameters();
-                    //turns off flashlight
+                //if(deviceHasCameraFlash) {
+
+
+                    //stänger av
                     if (isLight) {
                         parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
                         cam.setParameters(parameters);
                         cam.stopPreview();
                         isLight = false;
 
-                    //turns on flashlight
+                        //sätter på
                     } else {
                         parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                         cam.setParameters(parameters);
                         cam.startPreview();
                         isLight = true;
                     }
+               // }
 
             }
         });
@@ -88,11 +84,5 @@ public class MainActivity extends AppCompatActivity {
             //VibrationEffect vibrationEffect = Parcelable.Creator<VibrationEffect>();
             vibrator.vibrate(pattern, -1); //DEPRECATED METHOD :((( pls find the correct way to do dis
         }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.i("tag","access to camera is OK");
     }
 }

f27735f3aba5540d49cfa141814cf30093390f87
tobiasdolsson <tobias.d.olsson@hotmail.com>
2017-11-20 17:43:44 +0100

    Added save button to save spinner states

--- app/src/main/java/com/example/adrian/vibrationapp/BrandlarmSettings.java
+++ app/src/main/java/com/example/adrian/vibrationapp/BrandlarmSettings.java
@@ -1,18 +1,22 @@
 package com.example.adrian.vibrationapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class BrandlarmSettings extends AppCompatActivity {
-
+        Spinner vibrationSpinner;
+        int test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_brandlarm_settings);
-
-        Spinner vibrationSpinner = findViewById(R.id.vibration_spinner);
+        vibrationSpinner = findViewById(R.id.vibration_spinner);
         String[] vibrationItems = new String[]{"No vibration", "Vibration 1", "Vibration 2", "Vibration 3"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, vibrationItems);
         vibrationSpinner.setPrompt("Välj vibration");
@@ -27,4 +31,30 @@ public class BrandlarmSettings extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+        SharedPreferences sharedPref = getSharedPreferences("FileName",MODE_PRIVATE);
+        int spinnerValue = sharedPref.getInt("brandlarmVibration",-1);
+        if(spinnerValue != -1) {
+            // set the selected value of the spinner
+            vibrationSpinner.setSelection(spinnerValue);
+        }
+    }
+
+    public void saveSettings(View view)
+    {
+        int userChoice = vibrationSpinner.getSelectedItemPosition();
+        SharedPreferences sharedPref = getSharedPreferences("FileName",0);
+        SharedPreferences.Editor prefEditor = sharedPref.edit();
+        prefEditor.putInt("brandlarmVibration",userChoice);
+        prefEditor.commit();
+
+        Intent intent = new Intent(BrandlarmSettings.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/adrian/vibrationapp/RingklockaSettings.java
+++ app/src/main/java/com/example/adrian/vibrationapp/RingklockaSettings.java
@@ -1,19 +1,22 @@
 package com.example.adrian.vibrationapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class RingklockaSettings extends AppCompatActivity {
-
+    Spinner vibrationSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ringklocka_settings);
 
         //get the spinner from the xml.
-        Spinner dropdown = (Spinner)findViewById(R.id.vibration_spinner);
+        vibrationSpinner = findViewById(R.id.vibration_spinner);
         //create a list of items for the spinner.
 
         String[] items = new String[]{"No vibration", "Vibration 1", "Vibration 2", "Vibration 3"};
@@ -21,14 +24,38 @@ public class RingklockaSettings extends AppCompatActivity {
         //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
         //Set prompt message
-        dropdown.setPrompt("Välj vibration");
+        vibrationSpinner.setPrompt("Välj vibration");
         //set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
+        vibrationSpinner.setAdapter(adapter);
 
         Spinner connectionSpinner = findViewById(R.id.connection_spinner);
         String[] connectionItems = new String[]{"IoT Ringklocka"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, connectionItems);
         connectionSpinner.setPrompt("Välj device");
         connectionSpinner.setAdapter(adapter2);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+        SharedPreferences sharedPref = getSharedPreferences("FileName",MODE_PRIVATE);
+        int spinnerValue = sharedPref.getInt("ringklockaVibration",-1);
+        if(spinnerValue != -1) {
+            // set the selected value of the spinner
+            vibrationSpinner.setSelection(spinnerValue);
+        }
+    }
+
+    public void saveSettings(View view)
+    {
+        int userChoice = vibrationSpinner.getSelectedItemPosition();
+        SharedPreferences sharedPref = getSharedPreferences("FileName",0);
+        SharedPreferences.Editor prefEditor = sharedPref.edit();
+        prefEditor.putInt("ringklockaVibration",userChoice);
+        prefEditor.commit();
+
+        Intent intent = new Intent(RingklockaSettings.this, MainActivity.class);
+        startActivity(intent);
     }
 }
